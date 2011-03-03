603a3179af0185a33e606f006d36d0342c7e8013
Tony Collen <tony@recursiveawesome.com>
2011-03-03 10:39:06 -0600

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RumOwc2011::Application.config.secret_token = 'e6b908afe732081c334045952981ef9d9f89423ca8bffac718292150f732083e74cef45a64a61d92a01f9bedf1d5dee9636ed49324b5755c4c3f8ec78e55a5ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RumOwc2011::Application.config.session_store :cookie_store, :key => '_rum-owc2011_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RumOwc2011::Application.config.session_store :active_record_store
