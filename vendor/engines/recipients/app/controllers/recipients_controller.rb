f2067d60ffc642deafc398110634cd395519401b
Mark Wagner <mwagner72@gmail.com>
2011-03-26 14:35:29 -0500

    added relation between recipients and payments

---
+++ vendor/engines/recipients/app/controllers/recipients_controller.rb
@@ -0,0 +1,30 @@
+class RecipientsController < ApplicationController
+
+  before_filter :find_all_recipients
+  before_filter :find_page
+
+  def index
+    # you can use meta fields from your model instead (e.g. browser_title)
+    # by swapping @page for @recipient in the line below:
+    present(@page)
+  end
+
+  def show
+    @recipient = Recipient.find(params[:id])
+
+    # you can use meta fields from your model instead (e.g. browser_title)
+    # by swapping @page for @recipient in the line below:
+    present(@page)
+  end
+
+protected
+
+  def find_all_recipients
+    @recipients = Recipient.find(:all, :order => "position ASC")
+  end
+
+  def find_page
+    @page = Page.find_by_link_url("/recipients")
+  end
+
+end
---
+++ vendor/engines/recipients/lib/generators/refinerycms_recipients_generator.rb
@@ -0,0 +1,6 @@
+class RefinerycmsRecipients < Refinery::Generators::EngineInstaller
+
+  source_root File.expand_path('../../../', __FILE__)
+  engine_name "recipients"
+
+end
