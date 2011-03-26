84a026ba39d3f4adf61536248e495e09285d8e73
Mark Wagner <mwagner72@gmail.com>
2011-03-26 14:39:01 -0500

    fixed merge issue

--- vendor/engines/recipients/app/models/recipient.rb
+++ vendor/engines/recipients/app/models/recipient.rb
@@ -3,10 +3,8 @@ class Recipient < ActiveRecord::Base
   acts_as_indexed :fields => [:name, :story, :year]
 
   validates :name, :presence => true, :uniqueness => true
-<<<<<<< HEAD
-  
-=======
+
   has_many :payments
->>>>>>> update-payment-model
+
   belongs_to :image
 end
