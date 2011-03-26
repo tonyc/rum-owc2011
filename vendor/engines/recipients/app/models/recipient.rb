d836c907f37abcd0128b67b328173ee7037623c3
Mark Wagner <mwagner72@gmail.com>
2011-03-26 17:03:20 -0500

    added payment total to recip

--- vendor/engines/recipients/app/models/recipient.rb
+++ vendor/engines/recipients/app/models/recipient.rb
@@ -7,4 +7,12 @@ class Recipient < ActiveRecord::Base
   has_many :payments
 
   belongs_to :image
+
+  def payment_total
+    sum = 0.00 + 0.00
+    #for self.payments.each do |payment|
+    #    sum = sum + payment.amount
+    #end
+    payments.inject(0) { |sum, payment| sum = sum + payment.amount }
+  end
 end
