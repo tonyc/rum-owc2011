60e1f3e29aba43d96cf0d905816821e7b90c7d8c
Tony Collen <tony@halogenlabs.com>
2011-03-27 02:08:22 -0500

    footer links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
   def active_tag(active)
     active ? "active" : "inactive"
   end
+  
+  def top_level_pages
+    @menu_pages.includes(:slugs).select { |p| p.parent_id.nil? }
+  end
+  
+  def top_level_pages_without_home
+    top_level_pages.reject { |p| p.title == "Home"}
+  end
+  
 end
