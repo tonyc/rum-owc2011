477718ef0433bbc65bd77fe34240e73adac542d4
Mark Wagner <mwagner72@gmail.com>
2011-03-26 20:33:27 -0500

    removed mailer

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "mwagner72",
-  :password             => "hotgeek2go",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
