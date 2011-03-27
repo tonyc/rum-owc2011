6da42a309f72da29c45b3920b2ecd649f804d98f
Mark Wagner <mwagner72@gmail.com>
2011-03-27 00:39:45 -0500

    updated applicants

--- vendor/engines/applicants/app/controllers/admin/applicants_controller.rb
+++ vendor/engines/applicants/app/controllers/admin/applicants_controller.rb
@@ -11,5 +11,21 @@ module Admin
       render :partial => 'applicants' if request.xhr?
     end
 
+    def edit
+      @applicant = Applicant.find(params[:id])
+      @statuses = Applicant::STATUSES
+      @note = @applicant.notes.build()
+    end
+
+    def update
+     # if params[:applicant][:note].present? Note.new(:note, :applicant => Applicant.find(params[:applicant]))
+      if @applicant.update_attributes(params[:applicant])
+        flash[:success] = "Success"
+        redirect_to applicants_path
+      else
+        flash[:alert] = "Failed to Update"
+        render 'edit'
+      end
+    end
   end
 end
