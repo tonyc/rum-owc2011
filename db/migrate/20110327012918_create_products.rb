c692c0890f83c833b6235b9a3625bfea123cc05a
Tony Collen <tony@halogenlabs.com>
2011-03-27 05:56:32 -0500

    comment out create_table statement to let production migrate

--- db/migrate/20110327012918_create_products.rb
+++ db/migrate/20110327012918_create_products.rb
@@ -1,22 +1,22 @@
 class CreateProducts < ActiveRecord::Migration
 
   def self.up
-    create_table :products do |t|
-      t.string :name
-      t.string :short_description
-      t.text :description
-      t.string :price
-      t.boolean :purchased
-      t.boolean :published
-      t.integer :transaction_id
-      t.integer :image_id
-      t.integer :position
+    # create_table :products do |t|
+    #   t.string :name
+    #   t.string :short_description
+    #   t.text :description
+    #   t.string :price
+    #   t.boolean :purchased
+    #   t.boolean :published
+    #   t.integer :transaction_id
+    #   t.integer :image_id
+    #   t.integer :position
+    # 
+    #   t.timestamps
+    # end
 
-      t.timestamps
-    end
-
-    add_index :products, :id
-    add_index :products, :image_id
+    # add_index :products, :id
+    # add_index :products, :image_id
 
     load(Rails.root.join('db', 'seeds', 'products.rb'))
   end
